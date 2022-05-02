2130f121ea14d9b014fcb8c327e35fe76d5349d9
ArthPatel0311 <81942849+ArthPatel0311@users.noreply.github.com>
2022-05-02 01:44:47 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Arth Patel on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
